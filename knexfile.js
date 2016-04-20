c7a78385181719d9c2eab631392fe5b882e98b68
Jake Lambert <jake@onahill.co>
2016-04-20 11:46:23 -0500

    knexfile initial

---
+++ knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+var db = require("./server/modules/config");
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: db.url,
+      user:     db.username,
+      password: db.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: db.url,
+      user:     db.username,
+      password: db.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
