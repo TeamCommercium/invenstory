ef213f7c794d1bee088f1fe3156e46cb5367208e
Jake Lambert <jake@onahill.co>
2016-04-20 11:46:35 -0500

    create global server config file

---
+++ server/modules/config.js
@@ -0,0 +1,5 @@
+module.exports.db = { url:      process.env.DB_URL || 'localhost',
+                    name:     process.env.DB_NAME || 'invenstory_db',
+                    username: process.env.DB_USER || null,
+                    password: process.env.DB_PASSWORD || null
+                  }
