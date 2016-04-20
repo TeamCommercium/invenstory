25439b55340830e6d842dfaf34e3b8d1478e9af8
Jake Lambert <jake@onahill.co>
2016-04-20 16:01:03 -0500

    api updates

--- server/server.js
+++ server/server.js
@@ -70,12 +70,10 @@ app.listen(webServer.port, ()=>{console.log("Listening on", process.env.PORT)})
  * @apiParam    {number}   inventory.purchase_price Purchase price of new product listing in USD.
  * @apiParam    {date}     inventory.purchase_date Purchase price of new product listing in USD.
  * @apiParam    {number}   inventory.quantity Inventory product listing.
- * @apiParam    {Boolean}  [inventory.shipped=false] Set shipped status.
  * @apiSuccess  {Object}   inventory New product listing.
  * @apiSuccess  {number}   inventory.id id of product listing.
  * @apiSuccess  {number}   inventory.product_id id of product listing.
  * @apiSuccess  {number}   inventory.purchase_price Purchase price of new product listing in USD.
- * @apiSuccess  {Boolean}  inventory.shipped New product listing shipped status.
  *
  * @apiDescription Endpoint to add inventory. The server will lookup (create if necessary) the associated product id based on the supplied
  */
@@ -91,19 +89,21 @@ app.listen(webServer.port, ()=>{console.log("Listening on", process.env.PORT)})
   *
   * @apiSuccess {Object[]} products Returns user's product listings.
   * @apiSuccess {String} products.id id of new product listing.
-  * @apiSuccess {string} products.title Quantity of new product listing.
-  * @apiSuccess {string} products.description Description of product listing.
-  * @apiSuccess {Object[]} products.inventory Listing of inventory.
-  * @apiSuccess {number} products.inventory Purchase of product listing in USD.
-  * @apiSuccess {number} products.sale_price Quantity of product listing in inventory.
-  * @apiSuccess {timestamp} products.sale_price_time Timestamp of when sale price was last checked.
-  * @apiSuccess {Boolean} products.shipped Shipped status of product listing.
+  * @apiSuccess {number} products.quantity Quantity of product listing in inventory.
+  * @apiSuccess {number} products.purchase_price Average purchase price of in stock inventory.
+  * @apiSuccess {string} products.amzn_title Quantity of new product listing.
+  * @apiSuccess {string} products.amzn_description Description of product listing.
+  * @apiSuccess {number} products.amzn_price Current price of product on Amazon.
+  * @apiSuccess {number} products.amzn_rank Current sales ranks of product on Amazon.
+  * @apiSuccess {number} products.amzn_weight Shipping weight of product.
+  * @apiSuccess {number} products.amzn_manuf Product manufacturer.
+  * @apiSuccess {timestamp} products.amzn_price_time Timestamp of when sale price was last checked.
   *
-  * @apiDescription Endpoint to add a new product.
+  * @apiDescription Endpoint to add a new product. Response parameters with the "amzn" prefix represent data retreived from the Amazon API.
   */
 
  /**
-  * @api {put} /product/update Update Product Listing
+  * @api {put} /inventory/ship/:id Update Product Listing
   * @apiName UpdateProduct
   * @apiGroup product
   * @apiUse restricted
@@ -112,16 +112,8 @@ app.listen(webServer.port, ()=>{console.log("Listening on", process.env.PORT)})
   *
   * @apiParam {Object} product Updated product to update.
   * @apiParam {string} product.id ID of product to update.
-  * @apiParam {string} product.title New product listing title.
-  * @apiParam {string} product.description New product listing description.
-  * @apiParam {number} product.quantity New quantity to update.
-  * @apiParam {boolean} product.shipped Set shipped status.
-  * @apiSuccess {Object} product Return updated product listing.
-  * @apiSuccess {string} product.id ID of updated product listing.
-  * @apiSuccess {number} product.title Title of updated product listing.
-  * @apiSuccess {string} product.description Description of updated product listing.
-  * @apiSuccess {number} product.quantity Quantity of updated product listing.
-  * @apiSuccess {Boolean} product.shipped Status of updated product listing.
+  * @apiParam {number} product.quantity quantity shipped.
+  * @apiSuccess {Object} product Return updated product listing. See /api/product/list.
   */
 
 /**
@@ -130,10 +122,10 @@ app.listen(webServer.port, ()=>{console.log("Listening on", process.env.PORT)})
  * @apiGroup product
  * @apiUse restricted
  *
- * @apiParam {string} id Product listing id to delete.
+ * @apiParam {string} id Product listing id whose inventory should be deleted delete.
  * @apiError (404 Not found) err Not found.
  * @apiSuccessExample success-response:
  *  HTTP/1.1 204 OK
  *
- * @apiDescription Endpoint to allow user to delete user's own product. For security purposes, an authorized deletion should return a 404, as well.
+ * @apiDescription Endpoint to allow user to delete all of their own inventory records for a product.
  */
