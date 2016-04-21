082b9c184d70b299631157fe1e32f6e1ea192c1c
Jake Lambert <jake@onahill.co>
2016-04-21 15:34:09 -0500

    seed data closes #29

--- seeds/invenstory_dev.js
+++ seeds/invenstory_dev.js
@@ -5,6 +5,7 @@ exports.seed = function(knex, Promise) {
     knex('users').del(),
     knex('inventory').del(),
     knex('products').del(),
+    knex('product_details').del(),
     knex('auth').del(),
 
     // Seed users entries
@@ -32,19 +33,60 @@ exports.seed = function(knex, Promise) {
     //Seed products
     knex('products').insert({
         id: 85,
-        sku: 'MBZ5',
-        isbn: '978-3-16-148410-0',
-        msrp: 35.12,
-        currency: 'USD',
-        amzn_title: ,
-        amzn_description,
         amzn_asin: 'B00UYNAGTI',
-        amzn_price: 19.99,
-        amzn_updated: , //what does look like?
-        amzn_thumb: "http://ecx.images-amazon.com/images/I/61Gt0B2E7tL.​_SL75_​.jpg",
-        amzn_sales_rank: 12821,
-        amzn_weight: 0.50,
+        amzn_title: "LEGO Superheroes Marvel's Ant-Man 76039 Building Kit",
+        amzn_description: 'I like LEGOs sooooo much.',
         amzn_manufacturer: 'LEGO',
+        amzn_weight: 0.50,
+        amzn_thumb_url: "http://ecx.images-amazon.com/images/I/61Gt0B2E7tL.​_SL75_​.jpg",
+        amzn_list_price: 35.12,
+        currency: 'USD',
+        fetch_date: '2016-04-21 19:33:57', //what does look like?
       }),
+
+      knex('product_details').insert({
+        id: 3,
+        product_id: 85,
+        amzn_price: 19.99,
+        amzn_sales_rank: 12821,
+        amzn_fetch_date: '2016-04-21 19:33:57', //UTC
+        }),
+
+      knex('product_details').insert({
+        id: 8,
+        product_id: 85,
+        amzn_price: 19.89,
+        amzn_fetch_date: '2016-04-21 18:33:57', //UTC
+        amzn_sales_rank: 12824,
+      }),
+
+      //seed inventory
+      knex('inventory').insert({
+        id: 1,
+        user_id: 2,
+        product_id: 85,
+        sku: "SKHUGH",
+        purchase_date: '2015-02-20 11:33:57',
+        purchase_price: 14.99,
+      }),
+      
+      knex('inventory').insert({
+        id: 2,
+        user_id: 2,
+        product_id: 85,
+        sku: "SKHUGH",
+        purchase_date: '2015-02-20 11:33:57',
+        purchase_price: 14.99,
+      }),
+
+      knex('inventory').insert({
+        id: 3,
+        user_id: 2,
+        product_id: 85,
+        sku: "SKHUGH",
+        purchase_date: '2015-02-20 11:33:57',
+        purchase_price: 14.99,
+        shipped: true,
+      })
   );
 };
