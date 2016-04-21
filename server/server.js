1849bb0c49e4a5a4e904046a4041fb6950851e88
Jake Lambert <jake@onahill.co>
2016-04-21 11:34:14 -0500

    remove web server port logic from server.js
    previously moved to config.js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ var authAPI = require('./api/auth_api.js')
 var inventoryAPI = require('./api/auth_api.js')
 var amazonMWS = require('./api/amazonMWS.js')
 
-
-process.env.PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(express.static(path.join(__dirname, '../dist')))
