3d61c79773144ab018e1fafb27ee8b4cef94f090
Christopher Hummel <chrishummelj@gmail.com>
2016-04-20 19:27:29 -0500

    add list matching products api call

--- server/api/amazonMWS.js
+++ server/api/amazonMWS.js
@@ -1,47 +1,59 @@
 var MWS = require ('mws-sdk-promises')
+var config = require ('../config')
 
 
 var client = new MWS.Client(accessKeyId, secretAccessKey, merchantId, {})
-var MarketplaceId = "ATVPDKIKX0DER";
+var MarketplaceId = "ATVPDKIKX0DER"
 
-function getServiceStatus(client, args) {
-  var req = MWS.Products.requests.GetServiceStatus();
-  req.set(args);
-  return client.invoke(req);
-}
 
 function getLowestOfferListingsForASIN(client, args) {
-  var req = MWS.Products.requests.GetLowestOfferListingsForASIN();
+  var req = MWS.Products.requests.GetLowestOfferListingsForASIN()
   req.set(args);
   return client.invoke(req);
 }
 
-exports.getAmazonStatus = function(req,res) {
-  getServiceStatus(client, {})
-    .then(function(RESULT){
-      console.log("--------");
-      res.send(JSON.stringify(RESULT));
-      console.log("--------");
-    })
-    .catch(function(error) {
-      console.error(error);
-      res.send(error)
-    })
+function listMatchingProducts(client, args) {
+  var req = MWS.Products.requests.ListMatchingProducts()
+  req.set(args)
+  return client.invoke(req)
 }
 
-exports.getLowestOffers = function(req,res) {
+
+//Maximum request quota: 20 requests; 
+//Restore rate: 10 requests every second 
+//Hourly request quota: 36000 requests per hour
+
+exports.getLowestOffers = function(req, res) {
   getLowestOfferListingsForASIN(client, {
     MarketplaceId: MarketplaceId,
     ItemCondition: 'NEW',
     ASINList: 'B00IDBUM2O',
   })
-    .then(function(RESULT){
-      console.log("--------");
-      res.send(JSON.stringify(RESULT));
-      console.log("--------");
+    .then(function(result){
+      res.send(JSON.stringify(result));
     })
     .catch(function(error) {
-      console.error(error);
       res.send(error)
     })
 }
+
+
+
+//Maximum request quota: 20 requests; 
+//Restore rate: One request every five seconds  
+//Hourly request quota: 720 requests per hour
+
+exports.listMatchingProducts = function(req, res) {
+  listMatchingProducts(client, {
+    MarketplaceId: MarketplaceId,
+    Query: 'B00IDBUM2O',
+  })
+    .then(function(result){
+      res.send(JSON.stringify(result));
+    })
+    .catch(function(error) {
+      res.send(error)
+    })
+}
+}
+
