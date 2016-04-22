6f3538680d923da3c9b3c087f9748ae4cc1010c3
Nathan Schwartz <nathanschwartz@Nathans-MacBook-Air.local>
2016-04-22 15:59:34 -0500

    removed unnecessary variable

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, Route, NotFoundRoute, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import HomeContainer from './containers/home.jsx'
