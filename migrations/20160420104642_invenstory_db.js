5d1e4f8090592070e0a5e437d56aa9c9d065631e
Jake Lambert <jake@onahill.co>
2016-04-23 11:35:58 -0500

    modify users table schema

--- migrations/20160420104642_invenstory_db.js
+++ migrations/20160420104642_invenstory_db.js
@@ -3,9 +3,15 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable("users", function (table) {
       table.increments('id'); // integer id
-      table.string("mws_auth_token").unique();
-      table.string("seller_id").unique();
-      table.string("mws_marketplace"); // in app, default to us, which is:  ATVPDKIKX0DER
+      table.string("mws_auth_token").unique()
+      table.string("seller_id").unique()
+      table.string("mws_marketplace") // in app, default to us, which is:  ATVPDKIKX0DER
+      table.string("amzn_profile_id").unique()
+      table.string("amzn_username")
+      table.string("amzn_email")
+      table.string("amzn_zip")
+      table.string("amzn_accessToken")
+      table.string("amzn_refreshToken")
       table.timestamps();
     }),
 
@@ -42,14 +48,6 @@ exports.up = function(knex, Promise) {
       table.integer('amzn_sales_rank');
       table.string('currency').defaultTo('USD');
       table.datetime('amzn_fetch_date'); //last time data was pulled
-    }),
-
-    knex.schema.createTable("auth", function(table) {
-      table.increments('id');
-      table.integer('user_id').notNullable().references('users.id');
-      table.string('strategy');
-      table.integer('expiration');
-      table.string('authToken');
     })
   ])
 };
@@ -61,7 +59,6 @@ exports.down = function(knex, Promise) {
       knex.schema.dropTable('products'),
       knex.schema.dropTable('product_details'),
       knex.schema.dropTable('inventory'),
-      knex.schema.dropTable('auth'),
     ]
   )
 };
