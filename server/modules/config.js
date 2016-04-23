34ea1b90c20670b11841ae0c3b424d826b17d6bc
Jake Lambert <jake@onahill.co>
2016-04-23 16:02:19 -0500

    refactored db bootstrapping

--- server/modules/config.js
+++ server/modules/config.js
@@ -1,9 +1,9 @@
-module.exports.db = { url:      process.env.DB_URL || 'localhost',
+exports.db = { url:      process.env.DB_URL || 'localhost',
                     name:     process.env.DB_NAME || 'invenstory_db',
                     username: process.env.DB_USER || null,
                     password: process.env.DB_PASSWORD || null
                   }
-module.exports.webServer = {
+exports.webServer = {
                     port: process.env.PORT || 8080
 }
 
@@ -11,18 +11,20 @@ exports.state = {
                     env: process.env.NODE_ENV || 'development'
 }
 
-module.exports.amazonEnv = {
+exports.amazonEnv = {
                     accessKeyId:      process.env.MWS_ACCESS_KEY_ID || null,
                     secretAccessKey:  process.env.MWS_SECRET_KEY || null,
                     merchantId:       process.env.MWS_MERCHANT_ID || null,
 }
 
-module.exports.amazonAuth = {
+exports.amazonAuth = {
                     clientId:      process.env.AUTH_CLIENT_ID || null,
                     clientSecret:  process.env.AUTH_CLIENT_SECRET || null,
                     callbackURL:   process.env.AUTH_CALLBACK_URL || "http://127.0.0.1:8080/auth/amazon/callback"
 }
 
-module.exports.jwtConfig = {
+exports.jwtConfig = {
                     secret: 'doobydoobydooo'
 }
+
+exports.db = require('knex')(require('../../knexfile.js')[exports.state.env])
