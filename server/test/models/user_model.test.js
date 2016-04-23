23327636e00c4b987ffb4f2e32a99a246bb2ee58
Jake Lambert <jake@onahill.co>
2016-04-23 16:14:37 -0500

    remove private functions from test

--- server/test/models/user_model.test.js
+++ server/test/models/user_model.test.js
@@ -3,24 +3,14 @@ var expect = require('chai').expect;
 var User = require(__dirname + '/../../models/user_model.js')
 
 describe('User model', function() {
-  describe('#getUserFromAmznId', function () {
+  describe('#findOrCreateUser', function () {
     it('should exist', function () {
-      expect(User.getUserFromAmznId).to.exist;
-    });
-  });
-  describe('#createUser', function () {
-    it('should exist', function () {
-      expect(User.createUser).to.exist;
+      expect(User.findOrCreateUser).to.exist;
     });
   });
   describe('#updateUser', function () {
     it('should exist', function () {
       expect(User.updateUser).to.exist;
-    });
-  });
-  describe('#addAuth', function () {
-    it('should exist', function () {
-      expect(User.addAuth).to.exist;
     });
   });
 });
