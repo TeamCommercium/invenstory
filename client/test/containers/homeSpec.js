8cc652f84e14e23f6d90583fb629709e3ef9c19e
Nathan Schwartz <nathan.schwartz95@gmail.com>
2016-04-24 18:28:34 -0500

    fixed console log

--- client/test/containers/homeSpec.js
+++ client/test/containers/homeSpec.js
@@ -13,7 +13,7 @@ import Home from '../../components/navbar'
 chai.use(chaiEnzyme())
 
 const wrapper = shallow(<HomeContainer />) // mount/render/shallow when applicable
-console.log(Object.keys(wrapper), "node stuff", wrapper.nodes[0].props)
+// console.log(Object.keys(wrapper), "node stuff", wrapper.nodes[0].props)
 describe('Client: containers/navbarSpec.jsx', function () {
   it('Should contain the Navbar component', function () {
     expect(wrapper).to.contain(<NavbarContainer/>)
