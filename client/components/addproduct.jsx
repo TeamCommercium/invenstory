95ea41be9fd79efa82d7ebcfb9e61ac7b77f4b9d
flickerbits <hughsuh@gmail.com>
2016-04-26 00:10:36 -0500

    add addProduct modal handlers

--- client/components/addproduct.jsx
+++ client/components/addproduct.jsx
@@ -1,47 +1,52 @@
 import React from 'react'
 import { Button, Input, Dialog } from 'react-toolbox'
+// import Dashboard from '../containers/dashboard'
 
 
 export default (props) =>
 
-  // handleChange(value){
-  //   console.log(arguments)
-  // }
-
   <div>
     <Dialog active={true} title="ADD PRODUCT">
+      {console.log("props:", props)}
       <Input 
         className=""
         type='text' 
         label='ASIN for product'
         name='amzn_asin'
-        // onChange={this.handleChange.bind(this)} 
+        required={true}
+        onChange={ props.handleAsin.bind(this) } 
       />
       <Input 
         className=""
         type='number' 
         label='Purchase Price'
         name='purchase_price'
-        // onChange={this.handleChange.bind(this)} 
+        onChange={ props.handlePrice.bind(this) } 
       />
       <Input 
         className=""
         type='number' 
         label='Quantity'
         name='purchase_quantity'
-        // onChange={this.handleChange.bind(this)} 
+        onChange={ props.handleQuantity.bind(this) } 
       />
       <Input 
         className=""
         type='date' 
         label='Date Purchased'
         name='purchase_date'
-        // onChange={this.handleChange.bind(this)} 
+        onChange={ props.handleDate.bind(this) } 
       />
-      <Button className=""
+      <Button
+        className=""
         label='Submit' raised floating
-        // onMouseUp={   }
+        onMouseUp={ props.handleSubmit.bind(this) }
       />
-
+      <Button
+        className=""
+        label='Cancel' raised floating
+        onMouseUp={ props.cancelModal.bind(this) }
+      />
     </Dialog>
   </div>
+// })
--- client/containers/dashboard.jsx
+++ client/containers/dashboard.jsx
@@ -14,6 +14,11 @@ export default class DashboardContainer extends React.Component{
     super(props)
     this.state = {
       tableData: store.getState().tableData
+      showModal: false,
+      amzn_asin: null,
+      purchase_price: null,
+      purchase_quantity: null,
+      purchase_date: null
     };
   }
 
@@ -28,6 +33,56 @@ export default class DashboardContainer extends React.Component{
   handleChange(value){
     console.log(arguments)
   }
+
+  handleModal(){
+    this.setState({ showModal: true });
+  }
+
+  // toggleModal(){
+  //   this.setState({ showModal: !this.state.showModal });
+  //   console.log("STATE (toggle)", this.state);
+  // }
+
+  handleAsin(val){
+    console.log("ASIN:", val);
+    this.setState({ amzn_asin: [val] });
+  }
+
+  handlePrice(val){
+    console.log("Price:", val);
+    this.setState({ purchase_price: [val] });
+  }
+
+  handleQuantity(val){
+    console.log("Quantity:", val);
+    this.setState({ purchase_quantity: [val] });
+  }
+
+  handleDate(val){
+    console.log("Date:", val);
+    this.setState({ purchase_date: [val] });
+  }
+
+  handleSubmit(){
+    this.setState({ showModal: !this.state.showModal });
+    
+    console.log("STATE", this.state);
+  }
+
+  cancelModal(){
+    this.setState({
+      showModal: false,
+      amzn_asin: null,
+      purchase_price: null,
+      purchase_quantity: null,
+      purchase_date: null
+    })
+    console.log("state:",this.state);
+  }
+
+  // handleCancel(){
+  //   this.setState({ showModal: !this.state.showModal });
+  // }
 
   render(){
 
@@ -47,11 +102,24 @@ export default class DashboardContainer extends React.Component{
         name='name'
         onChange={this.handleChange.bind(this)} 
       />
-      <Button className="styles__inlineButton___16AEc" label='Add Product' raised floating />
+      <Button 
+        className="styles__inlineButton___16AEc"
+        label='Add Product' raised floating
+        onMouseUp={this.handleModal.bind(this)}
+      />
       
       <Dashboard data={this.state.tableData}/>
       {this.props.children}
-      <Addproduct />
+      {this.state.showModal 
+        ? <Addproduct 
+            cancelModal={this.cancelModal.bind(this)}
+            handleAsin={this.handleAsin.bind(this)}
+            handlePrice={this.handlePrice.bind(this)}
+            handleSubmit={this.handleSubmit.bind(this)}
+            handleQuantity={this.handleQuantity.bind(this)}
+            handleDate={this.handleDate.bind(this)}
+          /> 
+        : null}
     </div>
   }
 }
