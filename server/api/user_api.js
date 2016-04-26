3ec9547fd4147d78b11c6ad6af776046282ac7ac
Christopher Hummel <chrishummelj@gmail.com>
2016-04-25 19:09:47 -0500

    complete user/about endpoint

--- server/api/user_api.js
+++ server/api/user_api.js
@@ -1,5 +1,6 @@
 var express = require('express')
-
+var User = require('../models/user_model.js')
+var log = require('../modules/utilities.js').log;
 
 var router = express.Router()
 
@@ -26,13 +27,26 @@ var router = express.Router()
  *  @apiName GetUserInfo
  *  @apiGroup user
  *  @apiUse restricted
- *  @apiSuccess (200)
  *
- *  @apiDescription 
- *  
+ *  @apiSuccess {Object}  user                User info
+ *  @apiSuccess {string}  user.amzn_username  Amazon username
+ *  @apiSuccess {string}  user.amzn_email     Amazon email
+ *  @apiSuccess {string}  user.amzn_zip       Amazon zipcode "XXXXX-XXXX"
+ *
+ *  @apiDescription Endpoint to pull a users information. The server will lookup current users information.
+ *
+ *  @apiError (400 Bad Request) Request must a registered user.
  */
 .get('/about', function(req, res) {
-
+  User.getUserProfileInfo(req.user.id)
+    .then(function(result){
+      log('user_api:38, succesful /user/about', result)
+      res.status(200).({user: result[0]})
+    })
+    .catch(function(err) {
+      log('error in getting user profile ', err)
+      res.status(400).send('Bad Request')
+    })
 })
 
 
