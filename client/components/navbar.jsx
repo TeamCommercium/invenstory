a3b8c2703a1a677aa17cb5371a1674d1dd8f6b7d
Christopher Hummel <chrishummelj@gmail.com>
2016-04-26 20:01:54 -0500

    center and right align settings, logout button

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -6,11 +6,11 @@ import { redirect, logout } from '../util/util'
 export default (props) => 
 <div>
   <AppBar>
-    <Navigation type='horizontal'>
+    <Navigation className='styles__navbar___VdiD-' type='horizontal'>
       <Button label='Home' onMouseUp={redirect('/#/home')} raised primary />
       <Button label='Dashboard' onMouseUp={redirect('/#/dashboard')} raised primary />
-      <Button label='Logout' onMouseUp={logout} raised primary />
-      <IconButton className='stupidButton' primary><img style={{width: 35, height: 35}} src="http://i.imgur.com/pVDjxpB.png" /></IconButton>
+      <IconButton className='styles__settingButton___MrjY-' primary><img style={{width: 35, height: 35}} src="http://i.imgur.com/pVDjxpB.png" /></IconButton>
+      <Button className='styles__logout___3o2E6' label='Logout' onMouseUp={logout} raised primary />
     </Navigation>
   </AppBar>
 </div>
