fc736e8dfac67f46539a2cd2dbfb43936a78fbde
flickerbits <hughsuh@gmail.com>
2016-04-27 09:28:43 -0500

    add seller_sku input field, refine error handling, add new logo, code cleanup

--- client/containers/dashboard.jsx
+++ client/containers/dashboard.jsx
@@ -20,13 +20,14 @@ export default class DashboardContainer extends React.Component{
       tableData: store.getState().tableData,
       showModal: false,
       asin: '',
+      seller_sku: '',
       purchase_price: '',
       purchase_date: '',
       quantity: '',
       err_asin: '',
       err_purchase_price: '',
       err_purchase_date: '',
-      err_quantity: ''
+      err_quantity: ''    
     };
 
     let component = this;
@@ -65,29 +66,42 @@ export default class DashboardContainer extends React.Component{
   }
 
   handleSubmit(){
-  // Need to refactor
     let inputErr = 0;
+    
+  // Need to refactor
     if (this.state.asin.length < 10) {
       this.setState({err_asin: "Must be 10 characters"});
       inputErr++;
     } else this.setState({err_asin: ''});
-    if (!this.state.purchase_price) {
-      this.setState({err_purchase_price: "Please enter purchase price"});
+
+    if (!this.state.seller_sku || this.state.seller_sku.length < 4) {
+      this.setState({err_seller_sku: "Must be between 4-30 characters"});
+      inputErr++;
+    } else this.setState({err_seller_sku: ''});
+
+    if (!this.state.purchase_price || this.state.purchase_price < 0) {
+      this.setState({err_purchase_price: "Please enter valid purchase price"});
       inputErr++;
     } else this.setState({err_purchase_price: ''});
+
     if (!this.state.quantity || this.state.quantity < 1) {
       this.setState({err_quantity: "Please enter valid quantity"});
       inputErr++;
     } else this.setState({err_quantity: ''});
+
+    // need to add purchase date check <= current date
     if (!this.state.purchase_date) {
       this.setState({err_purchase_date: "Please enter purchase date"});
       inputErr++;
     } else this.setState({err_purchase_date: ''});
+
     if (!inputErr) {
-      // this.setState({showModal: !this.state.showModal});
         let inventory = {};
+        // standardize user sku & asin inputs to all caps
+        inventory.seller_sku = this.state.seller_sku.toUpperCase();
         inventory.asin = this.state.asin.toUpperCase();
-        inventory.purchase_price = this.state.purchase_price;
+        // round purchase price to 2 decimals
+        inventory.purchase_price = Math.round(this.state.purchase_price * 100) / 100;
         inventory.purchase_date = this.state.purchase_date;
         inventory.quantity = this.state.quantity;
         addUserInventory(inventory);
@@ -103,6 +117,7 @@ export default class DashboardContainer extends React.Component{
     this.state = ({
       tableData: this.state.tableData,
       asin: '',
+      seller_sku: '',
       purchase_price: '',
       purchase_quantity: '',
       purchase_date: '',
@@ -139,10 +154,12 @@ export default class DashboardContainer extends React.Component{
             handleInput={this.handleInput.bind(this)}
             resetModal={this.resetModal.bind(this)}
             asin={this.state.asin}
+            seller_sku={this.state.seller_sku}
             purchase_price={this.state.purchase_price}
             quantity={this.state.quantity}
             purchase_date={this.state.purchase_date}
             err_asin={this.state.err_asin}
+            err_seller_sku={this.state.err_seller_sku}
             err_purchase_price={this.state.err_purchase_price}
             err_quantity={this.state.err_quantity}
             err_purchase_date={this.state.err_purchase_date}
