ebe294ca04f5a2750657ac30d1a51a0fff27b14d
Jake Lambert <jake@onahill.co>
2016-05-02 17:14:08 -0500

    unit test product model connects #35

--- server/test/models/products_model.test.js
+++ server/test/models/products_model.test.js
@@ -3,8 +3,12 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 const Products = require(__dirname + '/../../models/products_model.js')
-
+const db      = require('../../modules/config.js').db
 describe('Products model', function() {
+
+  beforeEach(function(){
+    db.seed.run()
+  })
 
   describe('#addProduct', function () {
 
@@ -13,7 +17,7 @@ describe('Products model', function() {
     })
 
     it('should return an integer (product id)', function(done) {
-      Products.addProduct({'amzn_asin': 'testasin'})
+      Products.addProduct({'amzn_asin': 'B004QV6YMW'})
         .then(function(result) {
           expect(result).to.be.a('number')
           done()
@@ -27,7 +31,7 @@ describe('Products model', function() {
       expect(Products.getProductId).to.exist;
     });
 
-    it('should return an array containing object with id', function(done) {
+    it('should return an array containing object with id property', function(done) {
       Products.getProductId('B00UYNAGTI')
         .then(function(result) {
           expect(result[0]).to.be.an('object')
@@ -41,12 +45,29 @@ describe('Products model', function() {
     it('should exist', function () {
       expect(Products.editProduct).to.exist;
     });
+    it('should change a value', function(done) {
+      Products.editProduct({id: 85, amzn_title: 'New Title'})
+        .then((result) => {
+          console.log('assert result', result)
+          assert(78, result)
+          done()
+        })
+    })
   });
 
   describe('#addProductDetail', function () {
     it('should exist', function () {
       expect(Products.addProductDetail).to.exist;
     });
+
+    it('should return new detail id', function(done) {
+      Products.addProductDetail({product_id:85,amzn_price_fbm:23.99, amzn_price_fba:24.99,
+        amzn_fetch_date:'2016-01-01 12:00'})
+        .then(function(result) {
+          expect(result).is.a('number')
+          done()
+        })
+    })
   });
 
   describe('#getProducts', function () {
