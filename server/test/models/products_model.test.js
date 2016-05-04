86afbfe8dbdc468c40dd4c31376624e5f89aceff
Jake Lambert <jake@onahill.co>
2016-05-04 11:18:49 -0500

    return promise in tests instead done

--- server/test/models/products_model.test.js
+++ server/test/models/products_model.test.js
@@ -17,11 +17,10 @@ describe('Products model', function() {
       expect(Products.addProduct).to.exist;
     })
 
-    it('should return an integer (product id)', function(done) {
-      Products.addProduct({'amzn_asin': 'B004QV6YMW'})
+    it('should return an integer (product id)', function() {
+      return Products.addProduct({'amzn_asin': 'B004QV6YMW'})
         .then(function(result) {
           expect(result).to.be.a('number')
-          done()
         })
     })
   });
@@ -32,12 +31,11 @@ describe('Products model', function() {
       expect(Products.getProductId).to.exist;
     });
 
-    it('should return an array containing object with id property', function(done) {
-      Products.getProductId('B00UYNAGTI')
+    it('should return an array containing object with id property', function() {
+      return Products.getProductId('B00UYNAGTI')
         .then(function(result) {
           expect(result[0]).to.be.an('object')
           assert(85, result[0].id)
-          done()
         })
     })
   });
@@ -46,12 +44,11 @@ describe('Products model', function() {
     it('should exist', function () {
       expect(Products.editProduct).to.exist;
     });
-    it('should change a value', function(done) {
-      Products.editProduct({id: 85, amzn_title: 'New Title'})
+    it('should change a value', function() {
+      return Products.editProduct({id: 85, amzn_title: 'New Title'})
         .then((result) => {
           console.log('assert result', result)
           assert(78, result)
-          done()
         })
     })
   });
@@ -61,12 +58,11 @@ describe('Products model', function() {
       expect(Products.addProductDetail).to.exist;
     });
 
-    it('should return new detail id', function(done) {
-      Products.addProductDetail({product_id:85,amzn_price_fbm:23.99, amzn_price_fba:24.99,
+    it('should return new detail id', function() {
+      return Products.addProductDetail({product_id:85,amzn_price_fbm:23.99, amzn_price_fba:24.99,
         amzn_fetch_date:'2016-01-01 12:00'})
         .then(function(result) {
           expect(result).is.a('number')
-          done()
         })
     })
   });
@@ -76,12 +72,11 @@ describe('Products model', function() {
       expect(Products.getProducts).to.exist
       expect(Products.getProducts).to.be.a('function')
     })
-    it('should resolve to an array', function(done) {
+    it('should resolve to an array', function() {
       let uid = 2;
-      Products.getProducts(uid)
+      return Products.getProducts(uid)
         .then(function(result){
           expect(result).to.be.an('array')
-          done()
         })
     })
   });
