745f5af25ddf09bd17b3f6a3240b04848598257e
Nathan Schwartz <nathan.schwartz95@gmail.com>
2016-05-07 17:30:45 -0500

    working on testing react components

--- client/test/containers/homeSpec.js
+++ client/test/containers/homeSpec.js
@@ -1,22 +1,27 @@
-// import React from 'react'
-// import { mount, render, shallow } from 'enzyme'
-// import chaiEnzyme from 'chai-enzyme'
-// import chai, { expect } from 'chai'
-// import { spy } from 'sinon'
-// import { List } from 'react-toolbox'
-// import {LineChart} from 'rd3'
+import React from 'react'
+import { mount, render, shallow } from 'enzyme'
+import chaiEnzyme from 'chai-enzyme'
+import chai, { expect } from 'chai'
+import { spy } from 'sinon'
+import Stuff from '../../components/addproduct'
 
-// import HomeContainer from '../../containers/home.jsx'
-// import Home from '../../components/navbar'
+chai.use(chaiEnzyme())
 
-// chai.use(chaiEnzyme())
 
-// const wrapper = shallow(<HomeContainer />) // mount/render/shallow when applicable
-// // console.log(Object.keys(wrapper), "node stuff", wrapper.nodes[0].props)
-//   // it('Should contain the LineChart component', function () {
-//   //   expect(wrapper).to.contain(<LineChart/>)
-//   // });
-//   // it('Should contain the List component', function () {
-//   //   expect(wrapper).to.contain(<List/>)
-//   // });
+const wrapper = shallow(<Stuff />) // mount/render/shallow when applicable
+// console.log(Object.keys(wrapper), "node stuff", wrapper.nodes[0].props)
+  // console.log(Object.keys(HomeContainer))
+
+describe('Testing shallow rendering of HomeContainer', function () {
+  it('should work', function () {
+    expect(true).to.equal(true)
+  });
+});
+
+  // it('Should contain the LineChart component', function () {
+  //   expect(wrapper).to.contain(<LineChart/>)
+  // });
+  // it('Should contain the List component', function () {
+  //   expect(wrapper).to.contain(<List/>)
+  // });
 // });
