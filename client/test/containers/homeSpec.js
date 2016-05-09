4a5794b23ee3f17dfd66b667bdd5653fa59c4869
Nathan Schwartz <nathan.schwartz95@gmail.com>
2016-05-09 16:30:06 -0500

    homeSpec not ready

--- client/test/containers/homeSpec.js
+++ client/test/containers/homeSpec.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import { mount, render, shallow } from 'enzyme'
-import chaiEnzyme from 'chai-enzyme'
-import chai, { expect } from 'chai'
-import { spy } from 'sinon'
-import Stuff from '../../components/addproduct'
+// import React from 'react'
+// import { mount, render, shallow } from 'enzyme'
+// import chaiEnzyme from 'chai-enzyme'
+// import chai, { expect } from 'chai'
+// import { spy } from 'sinon'
+// import Stuff from '../../components/addproduct'
 
-chai.use(chaiEnzyme())
+// chai.use(chaiEnzyme())
 
 
-const wrapper = shallow(<Stuff />) // mount/render/shallow when applicable
-// console.log(Object.keys(wrapper), "node stuff", wrapper.nodes[0].props)
-  // console.log(Object.keys(HomeContainer))
+// const wrapper = shallow(<Stuff />) // mount/render/shallow when applicable
+// // console.log(Object.keys(wrapper), "node stuff", wrapper.nodes[0].props)
+//   // console.log(Object.keys(HomeContainer))
 
-describe('Testing shallow rendering of HomeContainer', function () {
-  it('should work', function () {
-    expect(true).to.equal(true)
-  });
-});
+// describe('Testing shallow rendering of HomeContainer', function () {
+//   it('should work', function () {
+//     expect(true).to.equal(true)
+//   });
+// });
 
   // it('Should contain the LineChart component', function () {
   //   expect(wrapper).to.contain(<LineChart/>)
