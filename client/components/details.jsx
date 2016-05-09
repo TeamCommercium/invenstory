d5a1e5ddf4dfe41549a8749226371881f78bc444
flickerbits <hughsuh@gmail.com>
2016-05-09 15:18:21 -0500

    add components/ship.js, basic functionality of ship inventory modal

--- client/components/details.jsx
+++ client/components/details.jsx
@@ -3,7 +3,7 @@ import { Table } from 'reactable'
 import { Button, Input, Slider } from 'react-toolbox'
 import { Chart } from 'react-google-charts'
 
-export default ({historical, options, data, hideDetails, smartAdd, deleteAll, confirmShip, err_quantity, handleQuantityChange, quantity }) =>
+export default ({historical, options, data, hideDetails, smartAdd, deleteAll, confirmShip, err_quantity, handleQuantityChange, quantity, handleShipModal }) =>
 <div className="styles__detailDisplay___2K0QU">
   <img className="styles__detailImage___3CFNO" src={data.amzn_thumb_url} />
   <h3 className="styles__detailTitle___2N12_"> {data.amzn_title} </h3>
@@ -14,16 +14,11 @@ export default ({historical, options, data, hideDetails, smartAdd, deleteAll, co
   <Button className="styles__detailButton___1aYnt" label='Close' raised floating inverse onMouseUp={hideDetails} />
   <Button className="styles__detailButton___1aYnt" label='Add' raised floating primary onMouseUp={smartAdd.bind(null,data)} />
   <Button className="styles__detailButton___1aYnt" label='Delete all' raised floating primary onMouseUp={deleteAll.bind(null, data.id, data.quantity, data.seller_sku)} />
-  <Button className="styles__detailButton___1aYnt" label='Ship' raised floating primary onMouseUp={confirmShip.bind(null, data.id, data.seller_sku, data.amzn_price_fba || data.amzn_price_fbm)} />
-  </div>
+
+  <Button className="styles__detailButton___1aYnt" label='Ship' raised floating primary onMouseUp={handleShipModal.bind(this)} />
   
-  <div>
-    <div className="styles__detailSliderInfo___V2gla">TOT Cost: ${(data.avg_purchase_price * quantity).toFixed(2)}</div>
-    <div className="styles__detailSliderInfo___V2gla">TOT Value: ${(data.amzn_price_fba * quantity).toFixed(2)}</div>
-    <div className="styles__detailSliderInfo___V2gla">NET Gain: ${((data.amzn_price_fba - data.avg_purchase_price) * quantity).toFixed(2)}</div>
-    <div className="styles__detailSliderInfo___V2gla">ROI: {quantity ? data.profit : 0}%</div>
-  </div>
-  <Slider className="styles__detailSlider___317hh" snaps={true} pinned={false} min={0} max={data.quantity} step={1} editable value={quantity || 0} onChange={handleQuantityChange.bind(this)} />
+  </div>  
+
   <Chart className="styles__detailChart___1CgJr" chartType="LineChart" data={historical} options={options} />
 
 </div>
---
+++ client/components/ship.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button, Input, Dialog, Slider } from 'react-toolbox'
+
+export default (props) =>
+
+  <Dialog active={props.active}>
+    <div>
+      <div className="styles__detailSliderInfo___V2gla">TOT Cost: ${(props.data.avg_purchase_price * props.ship_quantity).toFixed(2)}</div>
+      <div className="styles__detailSliderInfo___V2gla">TOT Value: ${(props.data.amzn_price_fba * props.ship_quantity).toFixed(2)}</div>
+      <div className="styles__detailSliderInfo___V2gla">NET Gain: ${((props.data.amzn_price_fba - props.data.avg_purchase_price) * props.ship_quantity).toFixed(2)}</div>
+      <div className="styles__detailSliderInfo___V2gla">ROI: {props.ship_quantity ? props.data.profit : 0}%</div>
+    </div>
+    <Slider className="styles__detailSlider___317hh" snaps={true} pinned={false} min={0} max={props.data.quantity} step={1} editable value={props.ship_quantity || 0} onChange={props.handleQuantityChange.bind(this)} />
+    <Button 
+      className="styles__inlineButton___16AEc" label='Cancel' raised floating onMouseUp={props.handleShipModal.bind(this)}
+    />
+    <Button className="styles__detailButton___1aYnt" label='Ship' raised floating primary onMouseUp={props.confirmShip.bind(null, props.data.id, props.data.seller_sku, props.data.amzn_price_fba || props.data.amzn_price_fbm)} />
+
+  </Dialog>
+
