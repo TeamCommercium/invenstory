4060bfbebc054fac65565c41c2e4867901c800a1
Christopher Hummel <chrishummelj@gmail.com>
2016-05-10 12:22:14 -0500

    working setting page

--- server/api/user_api.js
+++ server/api/user_api.js
@@ -51,24 +51,23 @@ var router = express.Router()
 })
 
 /**
- *  @api {put}  /user/update  Return User Info from database
+ *  @api {put}  /user/update  Update User info in the database
  *
- *  @apiName GetUserInfo
+ *  @apiName UpdateUserInfo
  *  @apiGroup user
  *  @apiUse restricted
  *
  *  @apiSuccess {Object}  user                User info
- *  @apiSuccess {string}  user.amzn_username  Amazon username
- *  @apiSuccess {string}  user.amzn_email     Amazon email
- *  @apiSuccess {string}  user.amzn_zip       Amazon zipcode "XXXXX-XXXX"
+ *  @apiSuccess {string}  user.amzn_username  Username
+ *  @apiSuccess {string}  user.amzn_email     Email
+ *  @apiSuccess {string}  user.amzn_zip       Zipcode "XXXXX-XXXX"
  *
- *  @apiDescription Endpoint to pull a users information. The server will lookup current users information.
+ *  @apiDescription Endpoint to update a users information. The server will update this information in the database.
  *
  *  @apiError (400 Bad Request) Request must a registered user.
  */
 .put('/update', function(req, res) {
-  params = req.body
-  console.log('server /update params: ', params)
+  var params = req.body
   User.updateUserInfo(req.user.id, params)
     .then(function(result){
       log('update user result ',result)
