bec17722d99f700bfbb48fd9ef1dc598adf4e808
Nathan Schwartz <nathan.schwartz95@gmail.com>
2016-05-10 19:14:20 -0500

    Forgot to include some files

---
+++ client/store/propActions.jsx
@@ -0,0 +1,32 @@
+export default {
+  detail: {
+    UPDATE_DETAIL_DATA: true,
+    pending: false,
+    payload: undefined
+  },
+  authenticated: {
+    UPDATE_AUTHENTICATION: true,
+    pending: false,
+    payload: undefined
+  },
+  tableData: {
+    UPDATE_TABLE_DATA: true,
+    pending: false,
+    payload: undefined
+  },
+  graphData: {
+    UPDATE_GRAPH_DATA: true,
+    pending: false,
+    payload: undefined
+  },
+  tab: {
+    CHANGE_TAB: true,
+    pending: false,
+    payload: undefined
+  },
+  notifications: {
+    UPDATE_NOTIFICATIONS: true,
+    pending: false,
+    payload: undefined
+  }
+}
---
+++ client/store/subscribeTo.jsx
@@ -0,0 +1,29 @@
+import action from './propActions'
+
+/*
+  function subscribeTo
+  Takes a string and a callback as parameters.
+  Return's nothing.
+  Wraps the store's subscribe method and only calls your callback when
+  the most recentlychanged value matches the string you enterred as a property.
+
+  This is intended to prevent unneeded re-renders by only triggering when relevant.
+ */
+
+export function subscribeTo(property, callback){
+  let store = this;
+
+  if( ! action[property])
+    throw new Error(`You tried to subscribe to ${property} but you may have meant on of the following: detail, inventory, authenticated, tableData, notifications, graphData, tab`)
+  
+  store.subscribe(function(){
+
+    let tempState = store.getState();
+    let changed = tempState.lastChanged
+
+    if(action[property][changed])
+      callback(tempState)
+  })
+}
+
+
