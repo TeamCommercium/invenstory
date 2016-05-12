e2b77ba16dab9faba8c5b720aaf64d57fc8a0268
Christopher Hummel <chrishummelj@gmail.com>
2016-05-12 11:22:07 -0500

    finish how it works section

--- client/containers/login.jsx
+++ client/containers/login.jsx
@@ -19,26 +19,59 @@ export default () =><div>
     <Button className='styles__loginButton___2G3Fy styles__neutral___3J4KA' label='Sign in' onMouseUp={redirect('/auth/amazon')} raised floating />
   </div>
 </div>
+
+<div className='container'>
+  <div className='row text-center'>
+    <h2> </h2>
+  </div>
+</div>
+
+<hr />
+
 <div className='styles__instructions___2gKDE'>
   <h2>How It Works</h2>
 </div>
 <div className="container">
   <div className="row text-center">
     <div className="col-md-4">
-      Sign In With Your Amazon Account
+      <div className='styles__instructionTitle___2oR7c'>
+        Sign In With Your Amazon Account
+      </div>
       <div className='col-md-10 col-md-offset-1'>
         <a id='amazonButton' href='/auth/amazon'>
+          <img className='img-responsive center-block styles__amazonPrimeLogo___HELvx' src='./assets/amazon_logo.png' />
           <img className='img-responsive center-block' src='https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_312x64.png' />
         </a>
       </div>
     </div>
     <div className="col-md-4">
-      Add Products to Your Inventory
+      <div className='styles__instructionTitle___2oR7c'>
+        Add Products to Your Inventory
+      </div>
+      <div className='col-md-10 col-md-offset-1'>
+        <img className='img-responsive center-block styles__boxShadow___g0vg-' src='./assets/add_products.png' />
+      </div>
     </div>
     <div className="col-md-4">
-    Mark Products as Shipped When Ready To Sell
+      <div className='styles__instructionTitle___2oR7c'>
+        Mark Products as Shipped When Ready To Sell
+      </div>
+      <div className='col-md-10 col-md-offset-1'>
+        <img className='img-responsive center-block styles__boxShadow___g0vg-' src='./assets/ship_products.png' />
+      </div>
     </div>
   </div>
+</div>
 
+<div className="container">
+  <div className='row'>
+    <div className='col-md-12 styles__footer___LPd_I'>
+      <div className='row styles__footerOptions___3a1Gp'>
+        <div className='text-right'>Team Commercium</div>
+      </div>
+    </div>
+  </div>
 </div>
 </div>
+
+
