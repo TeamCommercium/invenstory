6a432abd99329d6b5783c99deeac2f6ce26616db
Christopher Hummel <chrishummelj@gmail.com>
2016-05-12 14:56:15 -0500

    responsive login page complete

--- client/containers/login.jsx
+++ client/containers/login.jsx
@@ -22,7 +22,20 @@ export default () =><div>
 
 <div className='container'>
   <div className='row text-center'>
-    <h2> </h2>
+    <h2> Track Your Amazon Inventory </h2>
+      <div className='col-md-6 styles__productsTableCont___6ouhs'>
+        <p>Up-to-date Amazon prices to help you decide when to list your products</p>
+        <div className=''>
+          <img className='styles__productsTable___QHKgG img-responsive center-block styles__boxShadow___g0vg-' src='./assets/products_table.png' />
+        </div>
+      </div>
+      <div className='col-md-6'>  
+        <p>View charts showing current performance and value </p>
+        <div className=''>
+          <img className='img-responsive center-block styles__boxShadow___g0vg-' src='./assets/products_charts.png' />
+        </div>
+      </div>
+    
   </div>
 </div>
 
@@ -34,10 +47,10 @@ export default () =><div>
 <div className="container">
   <div className="row text-center">
     <div className="col-md-4">
-      <div className='styles__instructionTitle___2oR7c'>
-        Sign In With Your Amazon Account
-      </div>
-      <div className='col-md-10 col-md-offset-1'>
+      <div className='col-md-10 col-md-offset-1 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2'>
+        <div className='styles__instructionTitle___2oR7c'>
+          Sign In With Your Amazon Account
+        </div>
         <a id='amazonButton' href='/auth/amazon'>
           <img className='img-responsive center-block styles__amazonPrimeLogo___HELvx' src='./assets/amazon_logo.png' />
           <img className='img-responsive center-block' src='https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_312x64.png' />
@@ -45,18 +58,18 @@ export default () =><div>
       </div>
     </div>
     <div className="col-md-4">
-      <div className='styles__instructionTitle___2oR7c'>
-        Add Products to Your Inventory
-      </div>
-      <div className='col-md-10 col-md-offset-1'>
+      <div className='col-md-10 col-md-offset-1 col-xs-8 col-xs-offset-2 styles__addMargin___2E8Ad'>
+        <div className='styles__instructionTitle___2oR7c'>
+          Add Products to Your Inventory
+        </div>
         <img className='img-responsive center-block styles__boxShadow___g0vg-' src='./assets/add_products.png' />
       </div>
     </div>
     <div className="col-md-4">
-      <div className='styles__instructionTitle___2oR7c'>
-        Mark Products as Shipped When Ready To Sell
-      </div>
-      <div className='col-md-10 col-md-offset-1'>
+      <div className='col-md-10 col-md-offset-1 col-xs-8 col-xs-offset-2 styles__addMargin___2E8Ad'>
+        <div className='styles__instructionTitle___2oR7c'>
+          Ship Products When Ready To Sell
+        </div>
         <img className='img-responsive center-block styles__boxShadow___g0vg-' src='./assets/ship_products.png' />
       </div>
     </div>
@@ -67,7 +80,13 @@ export default () =><div>
   <div className='row'>
     <div className='col-md-12 styles__footer___LPd_I'>
       <div className='row styles__footerOptions___3a1Gp'>
-        <div className='text-right'>Team Commercium</div>
+        <div className='col-md-4 text-center'>
+          <a href='https://github.com/TeamCommercium/invenstory'>
+            <img className='styles__githubLogo___2ajiE' src='./assets/Github_logo.png' />
+          </a>
+        </div>
+        <div className='col-md-4'></div>
+        <div className='col-md-4 text-center styles__teamCommercium___1kRdu'>Team Commercium</div>
       </div>
     </div>
   </div>
