TeamCommercium/invenstory

commit 7d2c23751a00797db16559d3924a74044fcf848d

add UTC to detail date string

client/containers/dashboard.jsx
@@ -72,7 +72,7 @@ export default class DashboardContainer extends React.Component{
             graphData: [ 
               ["Date", "Price"], 
               ...data[0].history.map((cur)=>
-                [ new Date(cur.amzn_fetch_date), cur.amzn_price_fba || cur.amzn_price_fbm ])
+                [ new Date(cur.amzn_fetch_date+" UTC"), cur.amzn_price_fba || cur.amzn_price_fbm ])
             ],
 
             options: {

commit 69e6e6616282c47997e5c8ab836b3a0612ab1935

fix reversion from 7d2c23751a00797db16559d3924a74044fcf848d
broke formatted date output from Postgres

client/containers/dashboard.jsx
@@ -72,7 +72,7 @@ export default class DashboardContainer extends React.Component{
             graphData: [ 
               ["Date", "Price"], 
               ...data[0].history.map((cur)=>
-                [ new Date(cur.amzn_fetch_date+" UTC"), cur.amzn_price_fba || cur.amzn_price_fbm ])
+                [ new Date(cur.amzn_fetch_date), cur.amzn_price_fba || cur.amzn_price_fbm ])
             ],
 
             options: {

server/models/products_model.js
@@ -67,7 +67,7 @@ var amazonMWS = require('../api/amazonMWS.js');
  */
 exports.addProduct = function (asin) {
   let now = new Date()
-  let insertDate = dateFormat(now, 'yyyy-mm-dd HH:MM:ss', true)
+  let insertDate = dateFormat(now, 'yyyy-mm-dd HH:MM:ss Z', true)
   log('Create product with ASIN:', asin)
   return db('products')
     .returning('id')

server/modules/amznPriceService.js
@@ -77,7 +77,7 @@ function amznPriceSvc() {
 function Batch (arrayObjs) {
   var storage = {};
 
-  let batchTime = this.batchTime = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss', true);
+  let batchTime = this.batchTime = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss Z', true);
   this.asins = asins;
   this.prepareInsert = prepareInsert;
 