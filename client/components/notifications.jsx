6fe7ee5675a2d0d127b99195a9696ac7beaed1f3
flickerbits <hughsuh@gmail.com>
2016-05-14 12:30:56 -0500

    full image on search results, fixes #279

--- client/components/notifications.jsx
+++ client/components/notifications.jsx
@@ -7,8 +7,7 @@ export default ({data, visitItem}) =>
   {Array.prototype.map.call(data, (cur, index)=>
     <ListItem
       key={index}
-      leftIcon={<Avatar style={{"border-radius": 0, "background": "white"}}><img src={cur.amzn_thumb_url}/></Avatar>}
-      // leftIcon={<img src={cur.amzn_thumb_url} width="45px" height="auto"/>}
+      leftIcon={<Avatar style={{"borderRadius": 0, "background": "white"}}><img src={cur.amzn_thumb_url}/></Avatar>}
       caption={cur.amzn_title}
       legend={"Current ROI: " + cur.profit + "%"}
       onClick={visitItem.bind(null, cur)}
