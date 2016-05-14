689f25036843f81b5fd0a5ef10f7fb6954db7f05
flickerbits <hughsuh@gmail.com>
2016-05-14 10:17:27 -0500

    new cancel button in detail view

--- client/components/details.jsx
+++ client/components/details.jsx
@@ -6,12 +6,12 @@ import { Chart } from 'react-google-charts'
 export default ({historical, options, data, hideDetails, smartAdd, deleteAll, confirmShip, err_quantity, handleQuantityChange, quantity, handleShipModal, handleDeleteModal}) =>
 <div className="styles__detailDisplay___2K0QU">
   <img className="styles__detailImage___3CFNO" src={data.amzn_thumb_url} />
-  <h3 className="styles__detailTitle___2N12_"> {data.amzn_title} </h3>
+  <h3 className="styles__detailTitle___2N12_" style={{display: "inline", "margin": "10px"}}> {data.amzn_title} </h3>
+    <Button className="styles__detailCancel___dK0rK" icon='clear' floating mini primary onMouseUp={hideDetails} />
   <div className="styles__detailDescription___2v665"> {data.amzn_description && data.amzn_description.slice(0,240)} </div>
   <div> Sales Rank: {data.amzn_sales_rank} </div>
   <div> Weight: {data.amzn_weight} lbs</div>
   <div className="styles__detailButtonsDiv___3qeKQ">
-    <Button className="styles__inlineButton___16AEc detailBut closeButton" label='Close' raised floating primary onMouseUp={hideDetails} />
     <Button className="styles__inlineButton___16AEc detailBut" label='Delete all' raised floating primary onMouseUp={handleDeleteModal.bind(this)} />
     <Button className="styles__inlineButton___16AEc detailBut" label='Add' raised floating primary onMouseUp={smartAdd.bind(null,data)} />
     <Button className="styles__inlineButton___16AEc detailBut" label='Ship' raised floating primary onMouseUp={handleShipModal.bind(this)} />
