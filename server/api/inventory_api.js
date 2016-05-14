aa7668278931689a746825ef988e56f0f846834b
Jake Lambert <jake@onahill.co>
2016-05-14 16:14:47 -0500

    remove inventory/list endpoint

--- server/api/inventory_api.js
+++ server/api/inventory_api.js
@@ -65,44 +65,6 @@ var router = express.Router()
     })
   }
 })
- /**
-  * @api {get} /inventory/list List User's Products
-  *
-  * @apiName GetProducts
-  * @apiGroup product
-  * @apiUse restricted
-  *
-  * @apiParam {string} [id] Product to list. If omitted, all users products are returned.
-  *
-  * @apiSuccess {Object[]} products Returns user's product listings.
-  * @apiSuccess {String} products.product_id id of new product listing.
-  * @apiSuccess {number} products.quantity Quantity of product listing in inventory.
-  * @apiSuccess {number} products.purchase_price Average purchase price of in stock inventory.
-  * @apiSuccess {string} products.amzn_title Name of product.
-  * @apiSuccess {string} products.amzn_description Description of product listing.
-  * @apiSuccess {number} products.amzn_price_fbm Current price of product on Amazon (fulfilled by merchant).
-  * @apiSuccess {number} products.amzn_price_fba Current price of product on Amazon (fulfilled by Amazon).
-  * @apiSuccess {number} products.amzn_rank Current sales rank of product on Amazon.
-  * @apiSuccess {number} products.amzn_weight Shipping weight of product in pounds.
-  * @apiSuccess {string} products.amzn_manuf Product manufacturer.
-  * @apiSuccess {timestamp} products.amzn_price_time Timestamp of when sale price was last checked.
-  *
-  * @apiDescription Endpoint to add a new product. Response parameters with the "amzn" prefix represent data retreived from the Amazon API.
-  */
-
-  .get('/list', function(req, res) {
-   let params = req.query || {}
-   params.user_id = req.user.id
-   log("Web service request to list inventory: ", params)
-   Inventory.getInventory(params)
-     .then(function(data) {
-         res.status(200).send(data)
-     })
-     .catch(function(err) {
-       log("An error occurred getting inventory: ", err)
-       res.status(400).send("Bad request")
-     })
-  })
   /**
   * @api {put} /inventory/ship Set inventory status to shipped
   * @apiName UpdateProduct
